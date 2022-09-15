cfbfb5d439c991b0a825e5bcc35711481202bc5f
minkyeongJ <jomk2221@naver.com>
2022-09-15 21:05:43 +0900

    Remove: 사용하지 않는 API 키 삭제

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -2,6 +2,5 @@ const CALENDAR_ID = "c_uifli5n6o7avp8390627v8rh08@group.calendar.google.com";
 const BASE_URL = "https://www.googleapis.com/calendar/v3";
 const CLIENT_ID =
   "674347257066-lt6c788iectm9m9pmcddh1jmae82qion.apps.googleusercontent.com";
-const API_KEY = "AIzaSyATy7QBShuy9Ks8eyB9dkjwEP8J3M5xCUs";
 
 export { CALENDAR_ID, CLIENT_ID, BASE_URL, API_KEY };
