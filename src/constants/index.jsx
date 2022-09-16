bc76cc8cde69ebb0498177bc2783619eca7fa2ab
minkyeongJ <jomk2221@naver.com>
2022-09-16 16:43:20 +0900

    Fix: 상수 수정

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,6 +1,9 @@
-const CALENDAR_ID = "c_uifli5n6o7avp8390627v8rh08@group.calendar.google.com";
-const BASE_URL = "https://www.googleapis.com/calendar/v3";
+const CALENDAR_ID = "428464b353fffa95b24941bd7fbc93f2b98991888bb63f78c20b5f5d12a4bf0f@group.calendar.google.com";
+const BASE_URL = "https://www.googleapis.com/calendar/v3/calendars/";
 const CLIENT_ID =
   "674347257066-lt6c788iectm9m9pmcddh1jmae82qion.apps.googleusercontent.com";
+const AUTHORIZE_URI = "https://accounts.google.com/o/oauth2/v2/auth";
+const SCOPE_LIST =
+  "openid email profile https://www.googleapis.com/auth/calendar";
 
-export { CALENDAR_ID, CLIENT_ID, BASE_URL, API_KEY };
+export { CALENDAR_ID, CLIENT_ID, BASE_URL, AUTHORIZE_URI, SCOPE_LIST };
